ffcc3e83ea8b77670fb6df29ea2a4ae1c5f76483
Flavio Pinnelli <flavio.pinnelli@gmail.com>
2025-03-01 18:19:31 +0100

    modifiche per capolavoro

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { invalidateAll } from "$app/navigation";
   import { onMount } from "svelte";
   import { Confetti } from "svelte-confetti";
   import toast from "svelte-french-toast";
@@ -9,18 +10,21 @@
   let guess_row = 0;
 
   let win: any = null;
+  let concluded = false;
 
-  onMount(() => {
+  function setUp() {
+    console.log(`La parola da indovinare è: ${word} `);
+    guesses = []
+    guess_row = 0
+    concluded = false;
     addEventListener("keyup", (event) => {
       const key = event.key;
       const regex = /^[a-zA-Z]$/;
-      console.log(key);
 
       if (regex.test(key)) handleKeyPress(key);
       else if (key === "Enter") handleEnter();
       if (key === "Backspace") {
         handelCancel();
-        console.log("asdas");
       }
     });
 
@@ -37,6 +41,10 @@
     }
 
     guesses = guesses;
+  }
+
+  onMount(() => {
+    setUp();
   });
 
   function handleKeyPress(key: string) {
@@ -61,29 +69,41 @@
   }
 
   function handleEnter() {
+    let valid_submit = true;
+
     for (let i = 0; i < 5; i++) {
-      if (guesses[guess_row][i].letter === word[i]) {
-        guesses[guess_row][i].color = "6BAA65";
-      } else if (word.includes(guesses[guess_row][i].letter)) {
-        guesses[guess_row][i].color = "C9B458";
-      } else {
-        guesses[guess_row][i].color = "797D7E";
+      if (guesses[guess_row][i].letter === "") {
+        valid_submit = false;
       }
     }
 
-    const winning = checkValidity();
-    if (winning) {
-      toast.success("Parola indovinata");
-      win = true;
-      return;
-    }
+    if (valid_submit) {
+      for (let i = 0; i < 5; i++) {
+        if (guesses[guess_row][i].letter === word[i]) {
+          guesses[guess_row][i].color = "6BAA65";
+        } else if (word.includes(guesses[guess_row][i].letter)) {
+          guesses[guess_row][i].color = "C9B458";
+        } else {
+          guesses[guess_row][i].color = "797D7E";
+        }
+      }
 
-    if (guess_row === 4) {
-      toast.error(`La parola era ${word}`);
-      return;
-    }
+      const winning = checkValidity();
+      if (winning) {
+        toast.success("Parola indovinata");
+        win = true;
+        concluded = true;
+        return;
+      }
 
-    guess_row++;
+      if (guess_row === 4) {
+        toast.error(`La parola era ${word}`);
+        concluded = true;
+        return;
+      }
+
+      guess_row++;
+    }
   }
 
   function checkValidity() {
@@ -97,26 +117,48 @@
 
     return valid;
   }
+
+  function startGameAgain() {
+    invalidateAll();
+    setUp();
+    concluded = false
+    win = null
+
+  }
 </script>
 
 <div class="h-full w-full flex justify-center items-center">
   <div class="flex flex-col">
-    <h1 class="text-center font-poppins font-bold text-5xl my-5">Wordle</h1>
+    <h1 class="text-center font-poppins font-bold text-5xl my-5">Verble</h1>
 
     {#each guesses as guess}
       <div class="flex">
         {#each guess as letter}
           <div
-            style="background-color: #{letter.color}"
+            style="background-color: #{letter.letter ? letter.color : "white"}"
             class="border rounded border-gray-300 m-2 w-16 h-16 flex items-center justify-center transition"
           >
-            <h1 class="font-poppins font-bold text-3xl uppercase">
+            <h1
+              class="font-poppins font-bold text-3xl uppercase"
+              style="color: {letter.color !== 'white' ? 'white' : 'black'}"
+            >
               {letter.letter}
             </h1>
           </div>
         {/each}
       </div>
     {/each}
+
+    <!-- {word} -->
+    <div class="my-5 w-full h-10 flex items-center justify-center">
+      {#if concluded}
+        <button
+          on:click={() => location.reload()}
+          class="px-5 py-2 bg-blue-500 text-white rounded active:scale-75 transition font-poppins text-xl"
+          >Lude iterum</button
+        >
+      {/if}
+    </div>
   </div>
 </div>
 
@@ -129,8 +171,8 @@
       y={[0, 0.1]}
       delay={[500, 2000]}
       infinite
-      duration="5000"
-      amount="200"
+      duration={5000}
+      amount={200}
       fallDistance="100vh"
     />
   </div>
